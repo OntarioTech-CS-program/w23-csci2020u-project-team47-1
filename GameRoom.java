4e008028f5b592600ba4203a3113c8f1b7f71b53
Ryan <Ryan.liu1@ontariotechu.net>
2023-04-14 00:59:25 -0400

    updated- Ryan

--- GameRoom.java
+++ GameRoom.java
@@ -7,9 +7,74 @@ public class GameRoom {
     private String game_code;
     private Map<String, Player> players = new HashMap<String, Player>();
     private Set<Session> sessions = new HashSet<>();
-    private Map<String, Performance> playerRanks = new HashMap<>();
 
     String gameRoomID;
+
+    public class Player {
+        private String id;
+        private String name;
+        private int gamesWon;
+        private int gamesLost;
+        private int rank;
+
+        public Player(String id, String name, int gamesWon, int gamesLost, int rank) {
+            this.id = id;
+            this.name = name;
+            this.gamesWon = gamesWon;
+            this.gamesLost = gamesLost;
+            this.rank = rank;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getGamesWon() {
+            return gamesWon;
+        }
+
+        public void setGamesWon(int gamesWon) {
+            this.gamesWon = gamesWon;
+            updateRank();
+        }
+
+        public int getGamesLost() {
+            return gamesLost;
+        }
+
+        public void setGamesLost(int gamesLost) {
+            this.gamesLost = gamesLost;
+            updateRank();
+        }
+
+        public int getRank() {
+            return rank;
+        }
+
+        public void setRank(int rank) {
+            this.rank = rank;
+        }
+
+        private void updateRank() {
+            int totalGames = gamesWon + gamesLost;
+            double winRate = 0;
+            if (totalGames != 0) {
+                winRate = (double) gamesWon / totalGames;
+            }
+            int newRank = (int) Math.round(winRate * 100);
+            setRank(newRank);
+        }
+    }
+
+
 
     public GameRoom(String game_code, Player player, Session session) {
         this.game_code = game_code;
@@ -41,7 +106,7 @@ public class GameRoom {
     public void addPlayer(Player player, Session session) {
         players.put(player.getId(), player);
         sessions.add(session);
-        setPlayerRank(player.getId(), player.getGamesWon(), player.getGamesLost());
+//        setPlayerRank(player.getId(), player.getGamesWon(), player.getGamesLost());
     }
 
     public boolean inGame(String playerID) {
@@ -57,7 +122,7 @@ public class GameRoom {
             Player player = players.get(playerID);
             player.setName(name);
         } else {
-            Player player = new Player(playerID, name, 0);
+            Player player = new Player(playerID, name, 0, 0, 1);
             addPlayer(player, session);
         }
     }
@@ -70,3 +135,4 @@ public class GameRoom {
         return sessions;
     }
 }
+
