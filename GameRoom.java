28aa15e1470e7e2965e15307e9d2133ac07e4312
Ryan <Ryan.liu1@ontariotechu.net>
2023-04-13 23:42:39 -0400

    Ryan- made players as object in GameRoom

--- GameRoom.java
+++ GameRoom.java
@@ -1,21 +1,31 @@
 package com.example.finalgroupproject;
+
 import jakarta.websocket.Session;
 import java.util.*;
+
 public class GameRoom {
     private String game_code;
-    private Map<String, String> players = new HashMap<String, String>();
+    private Map<String, Player> players = new HashMap<String, Player>();
     private Set<Session> sessions = new HashSet<>();
+    private Map<String, Performance> playerRanks = new HashMap<>();
+
     String gameRoomID;
 
-    public GameRoom(String game_code, String players, Session session) {
+    public GameRoom(String game_code, Player player, Session session) {
         this.game_code = game_code;
-        this.players.put(players, "");
+        this.players.put(player.getId(), player);
 
-        addPlayer(players, "", session);
+        addPlayer(player, session);
     }
+
+    public Map<String, Player> getPlayers() {
+        return players;
+    }
+
     public void setGame_code(String game_code) {
         this.game_code = game_code;
     }
+
     public void setGameRoomID(String gameRoomID) {
         this.gameRoomID = gameRoomID;
     }
@@ -27,32 +37,35 @@ public class GameRoom {
     public String getGameRoom() {
         return this.game_code;
     }
-    public Map<String, String> getPlayers() {
-        return players;
-    }
 
-    public void addPlayer(String playerID, String name, Session session) {
-        players.put(playerID, name);
+    public void addPlayer(Player player, Session session) {
+        players.put(player.getId(), player);
         sessions.add(session);
+        setPlayerRank(player.getId(), player.getGamesWon(), player.getGamesLost());
     }
 
     public boolean inGame(String playerID) {
         return players.containsKey(playerID);
     }
+
     public boolean isEmpty() {
         return players.isEmpty();
     }
 
     public void setUserName(String playerID, String name, Session session) {
         if (players.containsKey(playerID)) {
-            players.put(playerID, name);
+            Player player = players.get(playerID);
+            player.setName(name);
         } else {
-            addPlayer(playerID, name, session);
+            Player player = new Player(playerID, name, 0);
+            addPlayer(player, session);
         }
     }
+
     public void removePlayer(String playerID) {
         players.remove(playerID);
     }
+
     public Set<Session> getSessions() {
         return sessions;
     }
