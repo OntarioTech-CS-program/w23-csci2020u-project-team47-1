9c3c2e411d961f11d37dc43a085fdeca854be685
Ryan <Ryan.liu1@ontariotechu.net>
2023-04-15 21:59:30 -0400

    done

--- src/main/java/com/example/wschatserverdemo/HelloServlet.java
+++ src/main/java/com/example/wschatserverdemo/HelloServlet.java
@@ -5,24 +5,27 @@ import java.io.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+// Annotation to define the servlet
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
+    // Initializing the message variable
     private String message;
-
+    // Initializing the servlet
     public void init() {
         message = "Hello World!";
     }
-
+    // Processing the HTTP GET request and generating the response
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
 
-        // Hello
+        // Writing the response to the client
         PrintWriter out = response.getWriter();
         out.println("<html><body>");
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
+    // Closing the servlet
     public void destroy() {
     }
+
 }
